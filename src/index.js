c138a9d6069a102968bde186f2efd5f518cf9489
Mugilvannan Loganathan <mugilvannan_l@persistent.com>
2021-07-20 19:58:57 +0530

    Test

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
-
+import { HashRouter } from 'react-router-dom';
 ReactDOM.render(
-  <React.StrictMode>
+  <HashRouter>
     <App />
-  </React.StrictMode>,
+    </HashRouter>,
   document.getElementById('root')
 );
 
